16529d9f81cf6a5ffd5668a7c843bbf8e3d43f72
Kevin08M <Kevin08modise@gmail.com>
2026-04-30 23:17:30 +0200

    Refactor Minibus to extend Bus and add toString
    Refactored Minibus class to extend Bus and override toString method.

--- Minibus.java
+++ Minibus.java
@@ -1,28 +1,17 @@
 public class Minibus extends Bus {
     private String minibusId;
-    private int capacity;
-    private Booking[] bookings;
-    private int count;
 
     public Minibus(String minibusId, int capacity) {
+        super(minibusId, capacity); 
         this.minibusId = minibusId;
-        this.capacity = capacity;
-        this.bookings = new Booking[capacity];
-        this.count = 0;
     }
 
     public String getMinibusId() {
         return minibusId;
     }
 
-    public boolean hasSpace() {
-        return count < capacity;
-    }
-
-    public void addBooking(Booking booking) {
-        if (hasSpace()) {
-            bookings[count] = booking;
-            count++;
-        }
+    @Override
+    public String toString() {
+        return String.format("Minibus Id: %s, Capacity: %d", getMinibusId(), capacity);
     }
 }
